426c8f7b335c8d6ea4b6da9cc1fc269faaa5599d
Salih <salihcakmak700@gmail.com>
2023-02-07 23:14:10 +0300

    Initial commit

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -1,7 +1,7 @@
 // import url from 'url';
 
 export function getBaseUrl(req) {
-    return URL(`${req.protocol}://${req.get('host')}`);
+    return new URL(`${req.protocol}://${req.get('host')}`);
     // return url.format({
     //     protocol: req.protocol,
     //     host: req.get('host'),
